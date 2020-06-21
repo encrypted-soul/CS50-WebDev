64951e927b7da036245ad9036f797353e97b35e4
Gagan Aryan <gaganaryan19@gmail.com>
2020-06-21 15:45:57 +0530

    major updates in project 2

--- 2/project2/application.py
+++ 2/project2/application.py
@@ -14,28 +14,37 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 usernames = []
-chatrooms = []
+chatrooms = ['random', 'general']
 chats = []
 
 @app.route("/")
 def index():
     if "username" in session:
-        return render_template("chat.html", name=session["username"])
+        return render_template("chat.html", name=session["username"], chatrooms=chatrooms)
     return render_template("index.html")
 
 @app.route("/enterStatus", methods=["POST"])
 def enterStatus():
     name = request.form.get("name")
 
-    if name in usernames:
-        return "Please chose another name this name has already been taken."
-    else:
-        session["username"] = name 
+    if name not in usernames:
         usernames.append(name)
-        return redirect(url_for("chat", name=name))
+
+    session["username"] = name 
+    return redirect(url_for("chat", name=name))
 
 
 @app.route("/<name>")
 def chat(name):
-    return render_template("chat.html", name=name)
+    return render_template("chat.html", name=name, chatrooms=chatrooms)
 
+@app.route("/changeName")
+def changeName():
+    session.pop("username", None)
+    return render_template("index.html")
+
+@app.route("/create_channel_status", methods=["POST", "GET"])
+def create_channel_status():
+    channel_name = request.form.get("channel_name")
+    chatrooms.append(channel_name)
+    return render_template("chat.html", name=session["username"], chatrooms=chatrooms)
